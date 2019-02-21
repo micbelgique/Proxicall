9a788b2009c26d7740d231c69992eaef1765d2a1
Xavier Vercruysse <vercruysse.xavier@xvercruysse.tech>
2019-02-21 20:22:36 +0100

    Started ProxiCall.Web migration to nodejs

---
+++ ProxiCall.NodeJS/ProxiCall.Web/README.md
@@ -0,0 +1,3 @@
+﻿# ProxiCall.Web
+
+
---
+++ ProxiCall.NodeJS/ProxiCall.Web/routes/webhooks.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* Answer url for nexmo */
+router.get('/answer', function (req, res) {
+    const ncco = [{
+        action: 'talk',
+        text: "I am a node js web application in need of love"
+    }];
+
+    res.send(ncco);
+});
+
+/* Event url for nexmo */
+router.post('/event', function (req, res) {
+    //TODO do something with the received events
+    res.status(200).send();
+});
+
+module.exports = router;
