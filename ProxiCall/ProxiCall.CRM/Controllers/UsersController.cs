bc539f177808662369c8abe21e51b8b924e9321c
xvercruysse <xavier.vercruysse@mic-belgique.be>
2019-03-21 15:45:18 +0100

    Added creation of user by admin

---
+++ ProxiCall/ProxiCall.CRM/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Proxicall.CRM.Models;
+
+namespace Proxicall.CRM.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IEmailSender _emailSender;
+
+        public UsersController(UserManager<IdentityUser> userManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _emailSender = emailSender;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("UserName,Email,Password,ConfirmPassword")] NewUser userForm)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = userForm.UserName, Email = userForm.Email };
+                var result = await _userManager.CreateAsync(user, userForm.Password);
+                if (result.Succeeded)
+                {
+                    //TODO add role combobox in view
+                    await _userManager.AddToRoleAsync(user, "User");
+                    //TODO send email to user
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            //TODO add view for new user details -> username, email, role, isConfirmed
+            return View();
+        }
+    }
+}
