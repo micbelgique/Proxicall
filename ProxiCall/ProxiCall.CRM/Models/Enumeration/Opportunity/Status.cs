78acd49263211ba0675ce25ecee64b1ba532106b
Melissa <melissa.fontesse@hotmail.com>
2019-03-21 12:57:30 +0100

    Opportunity WIP

---
+++ ProxiCall/ProxiCall.CRM/Models/Enumeration/Opportunity/Status.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proxicall.CRM.Models.Enumeration.Opportunity
+{
+    public class Status : Enumeration
+    {
+        public static Status Open = new OpenStatus();
+        public static Status Closed = new ClosedStatus();
+        public static Status Canceled = new CanceledStatus();
+
+        protected Status(int id, string name)
+        : base(id, name)
+        { }
+
+        private class OpenStatus : Status
+        {
+            public OpenStatus() : base(1, "Ouverte")
+            { }
+        }
+
+        private class ClosedStatus : Status
+        {
+            public ClosedStatus() : base(2, "Fermée")
+            { }
+        }
+
+        private class CanceledStatus : Status
+        {
+            public CanceledStatus() : base(3, "Annulée")
+            { }
+        }
+    }
+}
---
+++ ProxiCall/ProxiCall.CRM/Models/Opportunity.cs
@@ -0,0 +1,30 @@
+﻿using Proxicall.CRM.Models.Enumeration.Opportunity;
+using ProxiCall.CRM.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proxicall.CRM.Models
+{
+    public class Opportunity
+    {
+        [Key]
+        public string Id { get; set; }
+
+        //[Display(Name = "Owner")]
+        //public string OwnerId { get; set; }
+        //public Account Owner { get; set; }
+
+        [Display(Name = "Lead")]
+        public string LeadId { get; set; }
+        public Lead Lead { get; set; }
+
+        [Display(Name = "Product")]
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public DateTime EstimatedCloseDate { get; set; }
+        public string Comments { get; set; }
+        public Status Status { get; set; }
+        public Confidence Confidence { get; set; }
+    }
+}
