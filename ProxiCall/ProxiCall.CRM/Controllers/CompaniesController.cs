2d0d86e257bee63d64a02ecfb75c870db18b7ca0
Melissa <melissa.fontesse@hotmail.com>
2019-03-21 12:02:56 +0100

    Fixed using

--- ProxiCall/ProxiCall.CRM/Controllers/CompaniesController.cs
+++ ProxiCall/ProxiCall.CRM/Controllers/CompaniesController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProxiCall.CRM.Models;
 using Proxicall.CRM.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ProxiCall.CRM.Controllers
 {
