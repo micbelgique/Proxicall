01af503aee558fa7e4bc427f4ca6f7b5bb05e713
xvercruysse <xavier.vercruysse@mic-belgique.be>
2019-03-22 16:56:10 +0100

    Qd;in cqn creqte q user

--- ProxiCall/ProxiCall.CRM/Controllers/UsersController.cs
+++ ProxiCall/ProxiCall.CRM/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -38,22 +39,103 @@ namespace Proxicall.CRM.Controllers
         {
             if(ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = userForm.Email, Email = userForm.Email };
-                var result = await _userManager.CreateAsync(user, userForm.Password);
+                var user = new IdentityUser
+                {
+                    UserName = userForm.Email,
+                    Email = userForm.Email
+                };
+                var password = GenerateRandomPassword(_userManager.Options.Password);
+                var result = await _userManager.CreateAsync(user, password.ToString());
                 if (result.Succeeded)
                 {
                     var role = userForm.IsAdmin ? "Admin" : "User";
                     await _userManager.AddToRoleAsync(user, role);
-                    //TODO send email to user
-                    await _emailSender.SendEmailAsync(userForm.Email, "Finish creating your account", "Welcome to Proxicall CRM, the best CRM of all");
+                    //await _emailSender.SendEmailAsync(userForm.Email, "Finish creating your account", "One CRM to rule them all, One CRM to find them, One CRM to bring them all and in the darkness bind them.");
+
+                    await SendEmailConfirmation(user);
+                    //TODO add view for new user details -> username, email, role, isConfirmed
+                    return View("Index");
                 }
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
-            //TODO add view for new user details -> username, email, role, isConfirmed
             return View();
+        }
+
+        private async Task SendEmailConfirmation(IdentityUser user)
+        {
+            // For more information on how to enable account confirmation and password reset please 
+            // visit https://go.microsoft.com/fwlink/?LinkID=532713
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var callbackUrl = new Uri($"{Request.Scheme}://{Request.Host}/Identity/Account/ResetPassword?code={Uri.EscapeDataString(code)}").ToString();
+            var callbackUrlOld = Url.Page(
+                   "/Account/ResetPassword",
+                   pageHandler: null,
+                   values: new { code },
+                   protocol: Request.Scheme);
+            await _emailSender.SendEmailAsync(
+                user.Email,
+                "Welcome",
+                $"Welcome on Proxicall CRM</br>" +
+                $"<i>One CRM to rule them all, One CRM to find them, One CRM to bring them all and in the darkness bind them.</i></br>" +
+                $"Please choose your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+        }
+
+        /// <summary>
+        /// Generates a Random Password
+        /// respecting the given strength requirements.
+        /// </summary>
+        /// <param name="opts">A valid PasswordOptions object
+        /// containing the password strength requirements.</param>
+        /// <returns>A random password</returns>
+        public string GenerateRandomPassword(PasswordOptions opts = null)
+        {
+            if (opts == null) opts = new PasswordOptions()
+            {
+                RequiredLength = 8,
+                RequiredUniqueChars = 4,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireNonAlphanumeric = true,
+                RequireUppercase = true
+            };
+
+            string[] randomChars = new[] {
+                "ABCDEFGHJKLMNOPQRSTUVWXYZ",    // uppercase 
+                "abcdefghijkmnopqrstuvwxyz",    // lowercase
+                "0123456789",                   // digits
+                "!@$?_-"                        // non-alphanumeric
+            };
+            Random rand = new Random(Environment.TickCount);
+            List<char> chars = new List<char>();
+
+            if (opts.RequireUppercase)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[0][rand.Next(0, randomChars[0].Length)]);
+
+            if (opts.RequireLowercase)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[1][rand.Next(0, randomChars[1].Length)]);
+
+            if (opts.RequireDigit)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[2][rand.Next(0, randomChars[2].Length)]);
+
+            if (opts.RequireNonAlphanumeric)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[3][rand.Next(0, randomChars[3].Length)]);
+
+            for (int i = chars.Count; i < opts.RequiredLength
+                || chars.Distinct().Count() < opts.RequiredUniqueChars; i++)
+            {
+                string rcs = randomChars[rand.Next(0, randomChars.Length)];
+                chars.Insert(rand.Next(0, chars.Count),
+                    rcs[rand.Next(0, rcs.Length)]);
+            }
+
+            return new string(chars.ToArray());
         }
     }
 }
