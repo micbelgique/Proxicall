8af30a78a1dd530a83a02e6356f6baf04cb6e45b
Melissa <melissa.fontesse@hotmail.com>
2019-04-18 14:28:08 +0200

    Fixed dialog to say the dates properly

--- ProxiCall/ProxiCall.Bot/Dialogs/SearchData/SearchLeadDataDialog.cs
+++ ProxiCall/ProxiCall.Bot/Dialogs/SearchData/SearchLeadDataDialog.cs
@@ -352,7 +352,7 @@ namespace ProxiCall.Bot.Dialogs.SearchData
                 for (int i = 0; i < crmState.Opportunities.Count; i++)
                 {
                     wantedData.Append(string.Format(CulturedBot.ListOpportunities,
-                        crmState.Opportunities[i].Product.Title, crmState.Opportunities[i].CreationDate?.ToShortDateString()));
+                        crmState.Opportunities[i].Product.Title, crmState.Opportunities[i].CreationDate?.ToString("dd MMMM")));
                     if (i == (numberOfOpportunities - 2))
                     {
                         wantedData.Append($" {CulturedBot.LinkWithAnd} ");
